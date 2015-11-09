a0f9848d4c1bdb173160e198d6389e77f1d71672
Jean-Luc Martin <jean-luc.martin@kandy.io>
2015-11-09 16:08:49 -0500

    added reducers for auth state

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export {default as constants} from './constants';
 export {default as createMiddleware} from './middleware';
 export {default as actions} from './actions';
+export {default as reducer} from './reducers/index';
---
+++ src/reducers/auth.js
@@ -0,0 +1,20 @@
+import {handleActions} from 'redux-actions';
+import constants from '../constants';
+
+const reducers = {};
+reducers[constants.LOGIN_FINISHED] = (state, action) => ({
+    currentUsername: action.payload.username,
+    isAuthenticated: true
+});
+
+reducers[constants.LOGOUT] = () => ({
+    currentUsername: '',
+    isAuthenticated: false
+});
+
+// This creates a reducer function made up of many reducers.
+// The key is the action type for each reducer. The `next`
+// function is for successful actions and `throw` is for
+// error actions. When a value is a function instead of
+// an object, the same function is used for success and failure
+export default handleActions(reducers, {});
