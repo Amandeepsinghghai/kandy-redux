4d743ba2b9e942e25972cdfaaf783d72af27cb65
Jean-Luc Martin <jean-luc.martin@kandy.io>
2015-11-12 13:51:59 -0500

    added tests for constants

---
+++ test/constants.spec.js
@@ -0,0 +1,23 @@
+import constants from '../src/constants';
+import _ from 'lodash';
+
+describe('constants', function() {
+    it('should all start with the same prefix', function() {
+        const prefix = '@@kandy-redux/';
+
+        _.forEach(constants, (constant) => {
+            var startsWithPrefix = constant.indexOf(prefix) === 0;
+            expect(startsWithPrefix).to.be.true;
+        });
+    });
+
+    it('should be individually unique', function() {
+        // parsed constants will go into this hashmap
+        var parsed = {};
+
+        _.forEach(constants, (constant) => {
+            expect(parsed[constant]).to.be.undefined;
+            parsed[constant] = true;
+        });
+    });
+});
