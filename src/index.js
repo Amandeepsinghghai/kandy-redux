bf51746c2882d2b3efb6e2aff67608f18fca6ebe
MichelSimonot <Mitch.Simonot@kandy.io>
2016-01-15 14:44:36 -0500

    Remove old export statement.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export {mediaErrors, default as constants} from './constants';
+export {default as constants} from './constants';
 export {default as createMiddleware} from './middleware/createMiddleware';
 export {default as actions} from './actions';
 export {default as reducer} from './reducers/index';
