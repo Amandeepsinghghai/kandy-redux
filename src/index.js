67784531cf36dab4d6e292e432a67098b813c142
MichelSimonot <Mitch.Simonot@kandy.io>
2016-01-15 14:35:12 -0500

    Export kandy.js' mediaErrors.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export {default as constants} from './constants';
+export {mediaErrors, default as constants} from './constants';
 export {default as createMiddleware} from './middleware/createMiddleware';
 export {default as actions} from './actions';
 export {default as reducer} from './reducers/index';
